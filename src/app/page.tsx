cd22dd61488887f75aefa36dd816a8072773bff4
Christian Tallgren <ctallgren@outlook.com>
2024-10-08 16:25:15 +0300

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockUrls = [
+  "https://utfs.io/f/XqDbyjH42STQJKCj70SI2hVFp6b3DiISr94QdPcnvwZUjCtz",
+  "https://utfs.io/f/XqDbyjH42STQkIRdmosVEgQM6PqUAw5Ny3lWSFi018jxmJRr",
+  "https://utfs.io/f/XqDbyjH42STQGfRSj53WcvBE4x3MHfdRqP0ZsJFlOYuiTSzb",
+  "https://utfs.io/f/XqDbyjH42STQQhvESFiPP4fD3YwsU0vO7juMnEh9ZrcqVIWl",
+  "https://utfs.io/f/XqDbyjH42STQCkEcVrKtjcgRsMSUhy5Qm2ezdvpVFbA7BK4L",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
       Hello (gallery in progress)
     </main>
   );
