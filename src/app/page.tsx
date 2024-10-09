c9cf9b7d926d141368ff4955aa1182b5fb74c392
Christian Tallgren <ctallgren@outlook.com>
2024-10-09 12:55:17 +0300

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+const Images = async () => {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-      Hello (gallery in progress)
+      <SignedOut>
+        <div className="text.2xl h-full w-full text-center">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
